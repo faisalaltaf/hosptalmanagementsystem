167f43203cec8229c4774124e90bf7cf48425676
faisalaltaf <altaffaisal60@gmail.com>
2021-05-25 13:00:04 +0500

    entre

--- resources/views/dashboard/doctor/register.blade.php
+++ resources/views/dashboard/doctor/register.blade.php
@@ -27,7 +27,7 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/img-01.png" alt="IMG">
+					<img src="{{asset('doctorlogin/images/Tosca Blue Online Doctor Consultation Instagram Post.png')}}" alt="IMG">
 				</div>
 
 				<form class="login100-form validate-form">
@@ -36,23 +36,45 @@
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email">
+						<input class="input100" type="text" name="name" placeholder="Doctor Name">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
+						
+						</span>
+					</div>
+				
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="hospital" placeholder="enter the hospital name">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+					
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="email" name="email" placeholder=" entre the Email ">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="password" placeholder="password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+						
 						</span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+						<input class="input100" type="password" name="cpassword" placeholder="Confirm Password">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
-							<i class="fa fa-lock" aria-hidden="true"></i>
+		
 						</span>
 					</div>
 					
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						<button type="submit " name="submit" class="login100-form-btn">
 							Login
 						</button>
 					</div>
